52e69ad9ff1b17a62c9db1999e653691baca909a
siddick <mebramsha@paypal.com>
2015-12-23 12:51:41 +0530

    Added dust template

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express'),
     session = require('express-session'),
     logger = require('morgan'),
+    adaro = require('adaro'),
     app = express();
-
 module.exports = app;
+
+app.engine('dust', adaro.dust({ cache: false }));
+app.set('view engine', 'dust');
 
 app.use(logger());
 app.use(express.static('public'));
